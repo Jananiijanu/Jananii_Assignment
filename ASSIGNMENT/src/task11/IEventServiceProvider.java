51ff8f6133d4973ad8a378f516280cd0835f1978
jananiiprabhakaran <143985540+Jananiijanu@users.noreply.github.com>
2025-04-15 23:03:14 +0530

    Delete ASSIGNMENT/src directory

--- ASSIGNMENT/src/task11/IEventServiceProvider.java
+++
@@ -1,7 +0,0 @@
-package task11;
-import java.util.List;
-public interface IEventServiceProvider {
-    Event createEvent(String name, String date, String time, int totalSeats, double price, String type, Venue venue);
-    List<Event> getEventDetails();
-    int getAvailableSeats(String eventName);
-}
--- ASSIGNMENT/src/task11/Movie.java
+++
@@ -1,6 +0,0 @@
-package task11;
-public class Movie extends Event {
-    public Movie(String name, String date, String time, int totalSeats, double price, String type, Venue venue) {
-        super(name, date, time, totalSeats, price, type, venue);
-    }
-}
